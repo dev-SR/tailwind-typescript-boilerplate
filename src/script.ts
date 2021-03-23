ba332a6bf736405bb5d26375441a78921a9f71f7
Soikat Rahman <hello.dev.sr@gmail.com>
2021-03-24 01:02:11 +0600

    first commit

--- src/script.ts
+++ src/script.ts
@@ -1,5 +1,2 @@
-const btn = document.getElementById('btn') as HTMLElement;
-const myText = document.getElementById('myText') as HTMLElement;
-btn.addEventListener('click', () => {
-   myText.classList.toggle('text-purple');
-});
+let s: string = 'Hello World';
+console.log(s);
